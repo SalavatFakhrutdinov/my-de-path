0837f30a46c869a38e4d5f2447e85b3ec96e2aa9
Salavat_Fakhrutdinov <salavatfahrutdinov@yandex.ru>
2026-03-05 22:58:27 +0300

    added formatter

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from utils import normalize_name
 
+
 def main():
     try:
-        user_input = input('Введите строку: ')
+        user_input = input("Введите строку: ")
 
         if user_input and user_input.strip():
             normalized = normalize_name(user_input)
             print(f"Нормализованная строка: '{normalized}'")
         else:
-            print('Ожидалась строка, получены только пробелы')
+            print("Ожидалась строка, получены только пробелы")
     except KeyboardInterrupt:
-        print('\nИнициализировано завершение программы')
+        print("\nИнициализировано завершение программы")
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -3,12 +3,17 @@ from typing import Union
 
 logger = logging.getLogger(__name__)
 
+
 def normalize_name(input_str: Union[str, any]) -> str:
     if not isinstance(input_str, str):
-        logger.warning(f"Предупреждение: ожидалась строка, получено {type(input_str)}. Вернулась пустая строка")
+        logger.warning(
+            f"Ожидалась строка, получено {type(input_str)}.Вернулась пустая строка"
+        )
         return ""
-    
+
     if len(input_str) > 100:
-        logger.info(f"Нормализованная строка большой длины ({len(input_str)} символов)")
-        
+        logger.info(
+            f"Строка большой длины ({len(input_str)} символов)"
+        )
+
     return " ".join(input_str.split()).lower()
