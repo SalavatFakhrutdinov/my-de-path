73932f8d934a2ce71c6381ec72e39419eab47c2a
Salavat_Fakhrutdinov <salavatfahrutdinov@yandex.ru>
2026-03-05 23:07:28 +0300

    added logging_config.py

---
+++ logging_config.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+from typing import Optional
+
+def configure_logging(
+    level: str = "INFO",
+    log_file: Optional[str] = None,
+    verbose: bool = False
+) -> None:
+    
+    handlers = [logging.StreamHandler(sys.stdout)]
+
+    if log_file:
+        handlers.append(logging.FileHandler(log_file, encoding='utf-8'))
+    
+    if verbose:
+        level = "DEBUG"
+
+    logging.basicConfig(
+        level=getattr(logging, level),
+        format='%(asctime)s | %(levelname)8s |%(name)s | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=handlers
+    )
