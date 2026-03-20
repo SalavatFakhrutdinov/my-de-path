aeaad26095d8883c5bffaafaae0dce08b0ea93a6
Salavat_Fakhrutdinov <salavatfahrutdinov@yandex.ru>
2026-03-21 00:59:08 +0300

    added __init__.py

---
+++ src/file_processor/__init__.py
@@ -0,0 +1,29 @@
+from file_processor.main import main, run_application, parse_arguments
+from file_processor.reader import read_json_streaming
+from file_processor.validator import validate_user, filter_adults, sort_by_age
+from file_processor.transformations import apply_transformations
+from file_processor.writer import write_csv
+from file_processor.constants import (
+    DEFAULT_MIN_AGE,
+    EXIT_SUCCESS,
+    EXIT_FAILURE,
+    EXIT_INTERRUPT
+)
+
+
+__all__ = [
+    "__version__",
+    "main",
+    "run_application",
+    "parse_arguments",
+    "read_json_streaming",
+    "validate_user",
+    "filter_adults",
+    "sort_by_age",
+    "apply_transformations",
+    "write_csv",
+    "DEFAULT_MIN_AGE",
+    "EXIT_SUCCESS",
+    "EXIT_FAILURE",
+    "EXIT_INTERRUPT",
+]
