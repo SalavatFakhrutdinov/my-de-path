073fd6148f4e23fc6cd6276c5cf519ac70391dce
Salavat_Fakhrutdinov <salavatfahrutdinov@yandex.ru>
2026-03-22 15:40:59 +0300

    added streaming logic and get_user_ids in extractor.py

--- src/file_processor/extractor.py
+++ src/file_processor/extractor.py
@@ -2,27 +2,19 @@ import json
 import logging
 from typing import List, Dict, Any, Optional, Iterator
 
-from file_processor.reader import retry, RETRYABLE_EXCEPTIONS
+from file_processor.reader import read_json_streaming, read_json_as_list
 
 logger = logging.getLogger(__name__)
 
 
-@retry(max_attempts=3, delay=1.0)
-def _read_json_file(filepath: str) -> List[Dict[str, Any]]:
-    with open(filepath, "r", encoding="utf-8") as file:
-        data = json.load(file)
-
-    if not isinstance(data, list):
-        raise ValueError(f"Ожидался JSON массив, получен {type(data).__name__}")
-    
-    return data
-
-
+"""
+Извлечение всех пользователей/заказов из файла
+"""
 def extract_users(filepath: str) -> Optional[List[Dict[str, Any]]]:
     logger.info(f"Извлечение пользователей из {filepath}")
 
     try:
-        users = _read_json_file(filepath)
+        users = read_json_as_list(filepath)
         logger.info(f"Извлечено {len(users)} пользователей")
         return users
     except Exception as e:
@@ -34,9 +26,28 @@ def extract_orders(filepath: str) -> Optional[List[Dict[str, Any]]]:
     logger.info(f"Извлечение заказов из {filepath}")
 
     try:
-        orders = _read_json_file(filepath)
+        orders = read_json_as_list(filepath)
         logger.info(f"Извлечено {len(orders)} заказов")
         return orders
     except Exception as e:
         logger.error(f"Ошибка извлечения заказов: {e}")
         return None
+    
+
+"""
+Извлечение пользователей/заказов потоком
+"""
+def extract_users_streaming(filepath: str) -> Iterator[Dict[str, Any]]:
+    logger.info(f"Потоковое извлечение пользователей из {filepath}")
+    yield from read_json_streaming(filepath)
+
+def extract_orders_streaming(filepath: str) -> Iterator[Dict[str, Any]]:
+    logger.info(f"Потоковое извлечение заказов из {filepath}")
+    yield from read_json_streaming(filepath)
+
+
+"""
+Извлечение множество ID пользователей
+"""
+def get_user_ids(users: List[Dict[str, Any]]) -> set:
+    return {user.get("id") for user in users if user.get("id") is not None}
